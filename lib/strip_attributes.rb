65c66cb6bc2e59739b27960ab339de8bcab4dda5
rmm5t <rmm5t@ce739968-150a-4080-b6a2-b8faa43cca6a>
2007-09-23 05:05:31 +0000

    cleanup and simplification
    git-svn-id: http://stripattributes.rubyforge.org/svn/trunk@10 ce739968-150a-4080-b6a2-b8faa43cca6a

--- lib/strip_attributes.rb
+++ lib/strip_attributes.rb
@@ -2,11 +2,10 @@ module StripAttributes
   # Strips whitespace from model fields and converts blank values to nil.
   def strip_attributes!(options = nil)
     before_validation do |record|
-      attr_names = record.attributes(options).keys
-      attr_names.each do |attr_name|
-        s = record[attr_name]
-        if s.respond_to?(:strip)
-          record[attr_name] = (s.blank?) ? nil : s.strip
+      attributes = record.attributes(options)
+      attributes.each do |attr, value|
+        if value.respond_to?(:strip)
+          record[attr] = (value.blank?) ? nil : value.strip
         end
       end
     end
