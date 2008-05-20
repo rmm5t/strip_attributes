937700bc9b8d981fc3ba2e2456b69b38ca2b17e7
Ryan McGeary <ryanongit@mcgeary.org>
2008-05-20 17:52:48 -0400

    Fix for edge Rails, because options were removed as an argument from ActiveRecord::Base#attributes

--- lib/strip_attributes.rb
+++ lib/strip_attributes.rb
@@ -2,12 +2,30 @@ module StripAttributes
   # Strips whitespace from model fields and converts blank values to nil.
   def strip_attributes!(options = nil)
     before_validation do |record|
-      attributes = record.attributes(options)
+      attributes = StripAttributes.narrow(record.attributes, options)
       attributes.each do |attr, value|
         if value.respond_to?(:strip)
           record[attr] = (value.blank?) ? nil : value.strip
         end
       end
+    end
+  end
+  
+  # Necessary because Rails has removed the narrowing of attributes using :only
+  # and :except on Base#attributes
+  def self.narrow(attributes, options)
+    if options.nil?
+      attributes
+    else
+      if except = options[:except]
+        except = Array(except).collect { |attribute| attribute.to_s }
+        attributes.except(*except)
+      elsif only = options[:only]
+        only = Array(only).collect { |attribute| attribute.to_s }
+        attributes.slice(*only)
+      else
+        raise ArgumentError, "Options does not specify :except or :only (#{options.keys.inspect})"
+      end    
     end
   end
 end
