6476dc3add183afa96de95f3ac432752d2ac1954
Ryan McGeary <ryanongit@mcgeary.org>
2008-12-30 15:28:31 -0500

    Added a shoulda macro, should_strip_attributes, for easier testing in your models

---
+++ shoulda_macros/macros.rb
@@ -0,0 +1,22 @@
+require 'shoulda/active_record'
+
+module StripAttributes
+  module Macros
+    def should_strip_attributes(*attributes)
+      klass = model_class
+      attributes.each do |attribute|
+        attribute = attribute.to_sym
+        should "strip whitespace from #{attribute}" do
+          object = get_instance_of(klass)
+          object.send("#{attribute}=", " string ")
+          object.valid?
+          assert_equal "string", object.send(attribute)
+        end
+      end
+    end
+  end
+end
+
+class Test::Unit::TestCase
+  extend StripAttributes::Macros
+end
