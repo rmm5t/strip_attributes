e61f0a9dadd0273fc0fbaf9aba7ecd3d50b36431
Ryan McGeary <ryanongit@mcgeary.org>
2009-07-27 00:16:18 -0400

    Fixed macros for latest versions of shoulda

--- shoulda_macros/macros.rb
+++ shoulda_macros/macros.rb
@@ -3,7 +3,7 @@ require 'shoulda/active_record'
 module StripAttributes
   module Macros
     def should_strip_attributes(*attributes)
-      klass = model_class
+      klass = respond_to?(:described_type) ? described_type : model_class
       attributes.each do |attribute|
         attribute = attribute.to_sym
         should "strip whitespace from #{attribute}" do
@@ -16,7 +16,7 @@ module StripAttributes
     end
 
     def should_not_strip_attributes(*attributes)
-      klass = model_class
+      klass = respond_to?(:described_type) ? described_type : model_class
       attributes.each do |attribute|
         attribute = attribute.to_sym
         should "not strip whitespace from #{attribute}" do
