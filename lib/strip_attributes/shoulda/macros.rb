ec7cd46056989f3927710dc2e06141f80b1e8c7b
Ryan McGeary <ryan@mcgeary.org>
2012-08-29 17:05:26 -0400

    Switched shoulda macros to use `subject`

--- lib/strip_attributes/shoulda/macros.rb
+++ lib/strip_attributes/shoulda/macros.rb
@@ -4,27 +4,23 @@ module StripAttributes
   module Shoulda
     module Macros
       def should_strip_attributes(*attributes)
-        klass = described_type
         attributes.each do |attribute|
           attribute = attribute.to_sym
           should "strip whitespace from #{attribute}" do
-            object = get_instance_of(klass)
-            object.send("#{attribute}=", " string ")
-            object.valid?
-            assert_equal "string", object.send(attribute)
+            subject.send("#{attribute}=", " string ")
+            subject.valid?
+            assert_equal "string", subject.send(attribute)
           end
         end
       end
 
       def should_not_strip_attributes(*attributes)
-        klass = described_type
         attributes.each do |attribute|
           attribute = attribute.to_sym
           should "not strip whitespace from #{attribute}" do
-            object = get_instance_of(klass)
-            object.send("#{attribute}=", " string ")
-            object.valid?
-            assert_equal " string ", object.send(attribute)
+            subject.send("#{attribute}=", " string ")
+            subject.valid?
+            assert_equal " string ", subject.send(attribute)
           end
         end
       end
