99af730ca61f6e341d58f2c1f58665da5f61c5af
Ryan McGeary <ryan@mcgeary.org>
2013-01-25 10:32:32 -0500

    Added negative matcher tests

--- test/matchers_test.rb
+++ test/matchers_test.rb
@@ -16,4 +16,22 @@ describe SampleMockRecord do
 
   must { strip_attributes(:stripped1, :stripped2, :stripped3) }
   wont { strip_attributes(:unstripped1, :unstripped2, :unstripped3) }
+
+  it "should fail when testing for strip on an unstripped attribute" do
+    begin
+      assert_must strip_attributes(:unstripped1)
+      assert false
+    rescue
+      assert true
+    end
+  end
+
+  it "should fail when testing for no strip on a stripped attribute" do
+    begin
+      assert_wont strip_attributes(:stripped1)
+      assert false
+    rescue
+      assert true
+    end
+  end
 end
