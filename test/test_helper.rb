bdd123e8bfda78f7eef38e20f198b014fb80b0f9
Ryan McGeary <ryan@mcgeary.org>
2013-03-24 19:01:43 -0400

    Added dependency scenarios to the build matrix
    Including Rails 4

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,9 @@ require "active_attr"
 require "strip_attributes"
 
 class Tableless
-  include ActiveAttr::Model
+  include ActiveAttr::BasicModel
+  include ActiveAttr::TypecastedAttributes
+  include ActiveAttr::Serialization
+
   include ActiveModel::Validations::Callbacks
 end
